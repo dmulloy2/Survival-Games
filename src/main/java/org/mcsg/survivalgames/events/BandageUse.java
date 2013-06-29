82a5c8aeba58ef447640f0a717eb56de8d6fcc92
Dan Mulloy <dmulloy2@live.com>
2013-06-29 19:32:37 -0400

    Fixed bandages

--- src/main/java/org/mcsg/survivalgames/events/BandageUse.java
+++ src/main/java/org/mcsg/survivalgames/events/BandageUse.java
@@ -15,11 +15,26 @@ public class BandageUse implements Listener {
 	public void onBandageUse(PlayerInteractEvent e) {
 		Player player = e.getPlayer();
 		if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-			if (player.getItemInHand() == new ItemStack(Material.PAPER)) {
-				player.getInventory().removeItem(new ItemStack(Material.PAPER, 1));
-				player.setHealth(e.getPlayer().getHealth() + 10);
-				player.sendMessage(ChatColor.GREEN + "You used a bandage and got 5 hearts.");
+			ItemStack stack = player.getItemInHand();
+			if (stack == null || stack.getType() == Material.AIR) {
+				return;
 			}
+			
+			if (stack.getType() == Material.PAPER) {
+				player.getItemInHand().setAmount(stack.getAmount() - 1);
+				
+				int amountToGive = amountToGive(player);
+				player.setHealth(player.getHealth() + amountToGive);
+				player.sendMessage(ChatColor.GREEN + "You used a bandage and got " + amountToGive + " hearts!");
+			}
+		}
+	}
+	
+	public int amountToGive(Player player) {
+		if ((player.getHealth() + 5) > 20) {
+			return (20 - player.getHealth());
+		} else {
+			return 5;
 		}
 	}
 }
