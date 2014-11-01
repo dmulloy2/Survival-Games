c6ae98b89d49acee1dfdeae368f4f543311de63d
Dan Mulloy <dmulloy2@live.com>
2014-11-01 13:56:18 -0400

    Don't dump data

--- src/main/java/net/dmulloy2/survivalgames/handlers/QueueHandler.java
+++ src/main/java/net/dmulloy2/survivalgames/handlers/QueueHandler.java
@@ -1,23 +1,16 @@
 package net.dmulloy2.survivalgames.handlers;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.logging.Level;
 
 import net.dmulloy2.survivalgames.SurvivalGames;
 import net.dmulloy2.survivalgames.types.BlockData;
 import net.dmulloy2.survivalgames.types.Game;
-import net.dmulloy2.util.Util;
 
 import org.bukkit.Location;
 import org.bukkit.block.Block;
@@ -38,12 +31,6 @@ public class QueueHandler {
         this.baseDir = new File(plugin.getDataFolder(), "ArenaData");
         if (!baseDir.exists())
             baseDir.mkdirs();
-
-        for (Game g : plugin.getGameHandler().getGames()) {
-            ensureFile(g.getID());
-        }
-
-        new DataDumper().runTaskTimer(plugin, 100, 100);
     }
 
     private static final List<EntityType> PERSISTENT = Arrays.asList(
@@ -51,8 +38,6 @@ public class QueueHandler {
     );
 
     public void rollback(final int id) {
-        loadSave(id);
-
         if (plugin.isDisabling()) {
             rollback(id, true, 0, 1, 0);
         } else {
@@ -78,63 +63,10 @@ public class QueueHandler {
         List<BlockData> dat = queue.get(data.getGameId());
         if (dat == null) {
             dat = new ArrayList<BlockData>();
-            ensureFile(data.getGameId());
         }
 
         dat.add(data);
         queue.put(data.getGameId(), dat);
-    }
-
-    public void ensureFile(int id) {
-        try {
-            File file = new File(baseDir, "Arena" + id + ".dat");
-            if (!file.exists())
-                file.createNewFile();
-        } catch (IOException ex) {
-            plugin.log(Level.WARNING, Util.getUsefulStack(ex, "creating data file for game " + id));
-        }
-    }
-
-    public class DataDumper extends BukkitRunnable {
-
-        @Override
-        public void run() {
-            for (int id : queue.keySet()) {
-                try {
-                    List<BlockData> data = queue.get(id);
-                    ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File(baseDir, "Arena" + id + ".dat")));
-
-                    out.writeObject(data);
-                    out.flush();
-                    out.close();
-                } catch (IOException ex) {
-                    plugin.log(Level.WARNING, Util.getUsefulStack(ex, "dumping data for game " + id));
-                }
-            }
-        }
-
-    }
-
-    public void loadSave(int id) {
-        ensureFile(id);
-
-        try {
-            ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File(baseDir, "Arena" + id + ".dat")));
-
-            @SuppressWarnings("unchecked")
-            List<BlockData> input = (List<BlockData>) in.readObject();
-
-            List<BlockData> data = queue.get(id);
-            if (data == null) {
-                data = new ArrayList<BlockData>();
-            }
-
-            data.addAll(input);
-            queue.put(id, data);
-            in.close();
-        } catch (Throwable ex) {
-            plugin.log(Level.WARNING, Util.getUsefulStack(ex, "loading save for game " + id));
-        }
     }
 
     public class Rollback extends BukkitRunnable {
--- src/main/java/net/dmulloy2/survivalgames/types/BlockData.java
+++ src/main/java/net/dmulloy2/survivalgames/types/BlockData.java
@@ -1,13 +1,16 @@
 package net.dmulloy2.survivalgames.types;
 
-import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import net.dmulloy2.util.MaterialUtil;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
 
-public class BlockData implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class BlockData implements ConfigurationSerializable {
     private String world;
     private Material prevmat, newmat;
     private MaterialData prevdata, newdata;
@@ -69,5 +72,36 @@ public class BlockData implements Serializable {
 
     public ItemStack[] getItems() {
         return items;
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public Map<String, Object> serialize() {
+        Map<String, Object> data = new LinkedHashMap<>();
+        data.put("world", world);
+        data.put("prevmat", prevmat.name());
+        data.put("newmat", newmat.name());
+        data.put("prevdata", prevdata.getData());
+        data.put("newdata", newdata.getData());
+        data.put("x", x);
+        data.put("y", y);
+        data.put("z", z);
+        data.put("gameid", gameid);
+        data.put("items", items);
+        return data;
+    }
+
+    @SuppressWarnings("deprecation")
+    public BlockData(Map<String, Object> data) {
+        this.world = (String) data.get("world");
+        this.prevmat = MaterialUtil.getMaterial((String) data.get("prevmat"));
+        this.newmat = MaterialUtil.getMaterial((String) data.get("newmat"));
+        this.prevdata = new MaterialData(prevmat, (byte) data.get("prevdata"));
+        this.newdata = new MaterialData(newmat, (byte) data.get("newdata"));
+        this.x = (int) data.get("x");
+        this.y = (int) data.get("y");
+        this.z = (int) data.get("z");
+        this.gameid = (int) data.get("gameid");
+        this.items = (ItemStack[]) data.get("items");
     }
 }
